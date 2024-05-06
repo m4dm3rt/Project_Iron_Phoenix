d96f64e971607e8998ade13c11c29bb39796c5e1
m4dm3rt <mert-cansin@hotmail.de>
2024-05-06 11:37:46 +0200

    [*] Fixed EnemySpawner

--- Source/Project_Iron_Phoenix/Project_Iron_Phoenix.Build.cs
+++ Source/Project_Iron_Phoenix/Project_Iron_Phoenix.Build.cs
@@ -8,7 +8,7 @@ public class Project_Iron_Phoenix : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
